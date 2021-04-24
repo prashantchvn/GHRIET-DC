59fb1d2c8c8b49cb9e80df997e2d4879a23d8652
aishivam <shivamhande0000@gmail.com>
2021-04-25 00:32:26 +0530

    about and slider component add

---
+++ src/Components/slider.component.js
@@ -0,0 +1,21 @@
+const Slider=()=>{
+
+    return(
+        <>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm">
+                One of three columns
+                </div>
+                <div class="col-sm">
+                One of three columns
+                </div>
+                <div class="col-sm">
+                One of three columns
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+export default Slider;
