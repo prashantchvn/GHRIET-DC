1bf59805875685e03e43b5712105d3cf660123e1
aishivam <shivamhande0000@gmail.com>
2021-04-24 19:31:49 +0530

    css and terminal code add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Nav from "./Components/nav.component";
 import Header from './Components/header.component';
+import Terminal from "./Components/terminal.component";
 
 function App() {
   return (
     <div className="App">
       
-     <Nav/>
-     <Header/>
-
+      <Nav/>
+      <Header/>
+      <Terminal/>
     </div>
   );
 }
---
+++ src/Components/terminal.component.js
@@ -0,0 +1,20 @@
+import { Component } from "react";
+
+export default class Terminal extends Component{
+    render(){
+        return(
+            <>
+            <div className="card terminal align-middle shadow p-3 mb-5 bg-body rounded">
+                <div className="card-header ">
+                <i className="fa fa-circle red" aria-hidden="true"></i>
+                <i className="fa fa-circle yellow" aria-hidden="true"></i>
+                <i className="fa fa-circle green" aria-hidden="true"></i>
+                </div>
+                <div className="card-body terminal-body">
+                    <p>GHRIET@DC :</p>
+                </div>
+            </div>
+            </>
+        )
+    }
+}
