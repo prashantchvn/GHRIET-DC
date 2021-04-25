757f239d1a10490c92a0f1d0475139cd9b8f3ec2
prashantchvn <prashantchvn@gmail.com>
2021-04-25 13:09:38 +0530

    competition card component added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Nav from "./Components/nav.component";
 import Header from './Components/header.component';
 import Terminal from "./Components/terminal.component";
+import codefest from "./img/codefest.jpg"
+import Competiton from "./Components/competiton"
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <Nav/>
       <Header/>
       <Terminal/>
+      <Competiton bg={codefest} name="codefest" link="#" about=" is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, "/>
     </div>
   );
 }
--- src/Components/header.component.js
+++ src/Components/header.component.js
@@ -12,7 +12,7 @@ export default class Header extends Component{
             <div className="row gx-5">
               <div className="col">
                 <div className="input-group input-group-lg">
-                  <input type="text" className="form-control cform" placeholder="Your Email" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+                  <input type="text" className="form-control cform" placeholder="Join us" aria-label="Recipient's username" aria-describedby="button-addon2"/>
                   <a href="#" className="btn btn-outline-secondary cbtn" type="button" id="button-addon2"><i className="fa fa-paper-plane" aria-hidden="true"></i>
                   </a>
                 </div>
